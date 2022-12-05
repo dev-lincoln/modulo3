f1a2496fe102b54d567a351366cca69f4ab59a3c
dev-lincoln <lincoln.dev@outlook.com>
2022-12-05 11:13:55 -0300

    att 2

---
+++ app-rota/rotas-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { useState } from 'react'
+import './App.css'
+import Page1 from './Pages/Page1'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      
+        
+    </div>
+  )
+}
+
+export default App
---
+++ app-rota/rotas-app/src/Pages/Page2.jsx
@@ -0,0 +1,9 @@
+const Page1 = () => {
+    return (
+        <div>
+            <h2>Página 2</h2>
+        </div>
+    )
+}
+
+export default Page2
